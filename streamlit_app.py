cfb4c0a26636ce70e9e2072f7b473ffc921b40b1
Vassily Petrousevitch <vasspetrousevitch@gmail.com>
2024-03-02 14:54:54 -0800

    basic streamlit app done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time as time
 
 def main():
     # Register your pages
@@ -26,21 +27,48 @@ def page_home(pages):
     friend_quote = "_'Cause you got a friend in me!_"
     st.markdown(friend_quote)
 
-    # Upload image option
-    st.file_uploader("Upload friendless image", 
+    # Upload image widget
+    uploaded_file = st.file_uploader("Upload friendless image", 
                      type=['png', 'jpg'], 
                      accept_multiple_files=False,
-                     key="upload-image",
                      help="Upload an image into which you want extra people added!")
     
+    # Display uploaded file
+    if uploaded_file:
+        st.image(uploaded_file, width=500)
+    else:
+        st.image("img/input/pug.jpeg", width=500)   # Default
+
+    # Button
     if st.button("Click for friends!", key="click-for-friends"):
         pages["Photos page"]()
 
 
 def page_photos():
-    st.write("Here are your new friends!")
+    # Load CSS (don't touch this)
+    with open('styles.css') as f:
+        css = f.read()
+
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+    # End load CSS
+
     
-    # Download resultant image
+    # Output 
+    with st.spinner("Making friends..."):
+        # GENERATE IMAGE HERE
+
+
+
+
+        st.write("Here are your new friends!")
+
+        # TODO: change this to result photo
+        st.image("img/output/pug.jpeg", width=500)
+
+    # Download resultant image TODO: fix
+    # st.download_button("Download friendly image", 
+    #                    data="output/pug.jpeg",
+    #                    mime="image/jpeg")
 
 
 if __name__ == "__main__":
